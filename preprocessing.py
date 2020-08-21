5b0dc203102ff9fd0fc346c28c30b5087619e1c6
Rohit <rohitsrao@gmail.com>
2020-08-21 14:17:31 +0200

    Added Created Material class with initializer

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,14 @@
 #This contains all classes required for preprocessing.
+
+#Creating a class for material
+class Material():
+
+    #Initializer
+    def __init__(self, name, E):
+
+        #Setting name of the material
+        self.name = name
+
+        #Setting the Young's Modulus of the material
+        self.E = E
+
