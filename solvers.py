3b30f5778796b049160fd23bdff9cb38b7839cb7
Rohit <rohitsrao@gmail.com>
2020-09-08 12:05:42 +0200

    Created NewtonRaphson class with __init__()

--- solvers.py
+++ solvers.py
@@ -1,3 +1,10 @@
 #This contains all classes for different kinds of solvers
 
+class NewtonRaphson():
+
+    def __init__(self, truss):
+
+        #Storing a reference to the Truss object being solved
+        self.truss = truss
+
 
